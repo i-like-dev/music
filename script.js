6b09ba8746bd011ebcb6a36981c2c33bb445f2f3
i-like-dev <136171623+i-like-dev@users.noreply.github.com>
2025-07-25 18:54:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,31 @@
-let player;
-let musicData = JSON.parse(localStorage.getItem("musicData")) || [];
-let password = "0910";
+let currentPlayer = document.getElementById("player");
+let volumeSlider = document.getElementById("volume");
 
-function login() {
-  const input = document.getElementById("password").value;
-  if (input === password) {
-    document.getElementById("login").classList.add("hidden");
-    document.getElementById("admin").classList.remove("hidden");
-    renderAdminList();
-  } else {
-    Swal.fire("錯誤", "密碼錯誤", "error");
-  }
-}
-
-function addSong() {
-  const url = document.getElementById("videoUrl").value;
-  const title = document.getElementById("videoTitle").value;
-  const videoId = extractVideoId(url);
-  if (videoId && title) {
-    musicData.push({ title, videoId });
-    localStorage.setItem("musicData", JSON.stringify(musicData));
-    renderAdminList();
-    renderPlaylist();
-  } else {
-    Swal.fire("錯誤", "請輸入有效連結與標題", "warning");
-  }
-}
-
-function extractVideoId(url) {
-  const match = url.match(/(?:youtube\.com.*[\?&]v=|youtu\.be\/)([^&#]+)/);
-  return match ? match[1] : null;
-}
-
-function renderAdminList() {
-  const list = document.getElementById("songList");
-  list.innerHTML = "";
-  musicData.forEach((song, index) => {
-    const li = document.createElement("li");
-    li.textContent = `${song.title}`;
-    li.onclick = () => {
-      musicData.splice(index, 1);
-      localStorage.setItem("musicData", JSON.stringify(musicData));
-      renderAdminList();
-      renderPlaylist();
-    };
-    list.appendChild(li);
+fetch("music.json")
+  .then(res => res.json())
+  .then(data => {
+    const playlistDiv = document.getElementById("playlist");
+    data.forEach(song => {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.innerHTML = `<strong>${song.title}</strong><br><small>${song.artist}</small>`;
+      card.onclick = () => play(song.url);
+      playlistDiv.appendChild(card);
+    });
   });
+
+function play(url) {
+  const embed = convertToEmbed(url);
+  currentPlayer.src = embed + "?autoplay=1";
 }
 
-function renderPlaylist() {
-  const playlist = document.getElementById("playlist");
-  playlist.innerHTML = "";
-  musicData.forEach((song, index) => {
-    const div = document.createElement("div");
-    div.className = "song";
-    div.textContent = song.title;
-    div.onclick = () => playVideo(song.videoId);
-    playlist.appendChild(div);
-  });
+function convertToEmbed(url) {
+  const videoId = url.split("v=")[1] || url.split("youtu.be/")[1];
+  return `https://www.youtube.com/embed/${videoId}`;
 }
 
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player("ytplayer", {
-    height: "360",
-    width: "640",
-    videoId: "",
-    events: {
-      onReady: () => renderPlaylist()
-    }
-  });
-}
-
-function playVideo(videoId) {
-  if (player && player.loadVideoById) {
-    player.loadVideoById(videoId);
-  }
-}
+volumeSlider.addEventListener("input", () => {
+  const iframe = currentPlayer;
+  const vol = volumeSlider.value;
+  iframe.contentWindow.postMessage(`{"event":"command","func":"setVolume","args":[${vol}]}`, "*");
+});
